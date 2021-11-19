f0e68a936cdd1d5155a3f90a3c95b1e97fbe39da
AmaraMed <mohamed.amara.1@esprit.tn>
2021-11-19 16:03:06 +0100

    AngularV1

---
+++ miniprojet4/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidemenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ miniprojet4/src/app/services/session.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { User } from '../Model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService {
+  user : User;
+  constructor() {
+    this.user=new User("test","test","Premium");
+   }
+   getUser() : User{
+
+     return this.user;
+   }
+   getSessionType():string{
+     if (this.user.badge != '')
+     {
+      if (this.user.badge === 'Fidele' || this.user.badge === 'Ordinaire' || this.user.badge === 'Premium'){
+        return 'USER';
+      }
+      else
+      return 'MODERATEUR';
+     }
+     return 'NL';
+   }
+}
