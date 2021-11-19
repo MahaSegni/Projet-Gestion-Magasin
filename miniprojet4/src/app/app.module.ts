fd5402d318e4745cf246f996fb5ae3a2dc5bd49c
MaalawiMahdi <mehdi.maalawi@esprit.tn>
2021-11-19 16:33:57 +0100

    update

--- miniprojet4/src/app/app.module.ts
+++ miniprojet4/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { SidemenuComponent } from './sidemenu/sidemenu.component';
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidemenuComponent
+    SidemenuComponent,
   ],
   imports: [
     BrowserModule,
