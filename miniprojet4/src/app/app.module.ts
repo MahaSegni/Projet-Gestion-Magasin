97811b5c2b61639d56046345953886f51d20cd95
Basbousa98 <emna.besbes@esprit.tn>
2021-12-05 09:43:02 +0100

    update GestionProduit

--- miniprojet4/src/app/app.module.ts
+++ miniprojet4/src/app/app.module.ts
@@ -6,6 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import { FooterComponent } from './footer/footer.component';
+import { CategorieProduitComponent } from './categorie-produit/categorie-produit.component';
+import { HttpClientModule} from "@angular/common/http";
+import { ProduitComponent } from './produit/produit.component';
+import { ReclamationComponent } from './reclamation/reclamation.component';
+import { ReponseReclamationComponent } from './reponse-reclamation/reponse-reclamation.component';
+
+import { FormCategorieproduitComponent } from './form-categorieproduit/form-categorieproduit.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormProduitComponent} from "./form-produit/form-produit.component";
+import { FormReclamationComponent } from './form-reclamation/form-reclamation.component';
+import { FormReponsereclamationComponent } from './form-reponsereclamation/form-reponsereclamation.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +24,21 @@ import { FooterComponent } from './footer/footer.component';
     NavbarComponent,
     SidemenuComponent,
     FooterComponent,
+    CategorieProduitComponent,
+    ProduitComponent,
+    ReclamationComponent,
+    ReponseReclamationComponent,
+    FormCategorieproduitComponent,
+    FormProduitComponent,
+    FormReclamationComponent,
+    FormReponsereclamationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
